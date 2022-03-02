e93490b70daa4942bed264b9b1323b73fab9d4b0
Soundarya1311 <soundaryakumar76@gmail.com>
2022-03-02 12:42:54 +0530

    usrmethod

--- src/tesst/Check.java
+++ src/tesst/Check.java
@@ -3,8 +3,12 @@ package tesst;
 
 public class Check {
 	
-	
-		
+	private static void usrname() {
+		System.out.println("KumarAppa");
+	}
+		public static void main(String[] args) {
+		usrname();	
+		}
 	}
 
 
