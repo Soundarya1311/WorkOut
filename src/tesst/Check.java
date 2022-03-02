8a173f751a2c60a535228cf35954aac0c584b59a
Soundarya1311 <soundaryakumar76@gmail.com>
2022-03-02 12:46:59 +0530

    passwordmethod

--- src/tesst/Check.java
+++ src/tesst/Check.java
@@ -3,8 +3,13 @@ package tesst;
 
 public class Check {
 	
-	
-		
+	private static void passWord() {
+		System.out.println("jhh");
+
+	}
+		public static void main(String[] args) {
+			passWord();
+		}
 	}
 
 
